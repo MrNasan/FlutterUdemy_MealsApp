75c7350e9c19d3a387853c76c2697bbeb4706cf5
Nasan Soonthondachar <63160070@go.buu.ac.th>
2022-04-22 15:13:55 +0700

    Create another screen for tap test

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -11,7 +11,7 @@ class CategoriesScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Delimeal',
+          'DeliMeal',
         ),
       ),
       body: GridView(
--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,11 @@ class MyApp extends StatelessWidget {
               bodyText2: const TextStyle(
                 color: Color.fromRGBO(20, 51, 51, 1),
               ),
-              headline6:
-                  const TextStyle(fontSize: 24, fontFamily: 'RobotoCondensed'),
+              headline6: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
             ),
       ),
       home: const CategoriesScreen(),
